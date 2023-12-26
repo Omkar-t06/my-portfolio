f429f9ba50524cda7425a38f48c15f8652a0d4b7
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2023-12-26 13:01:46 +0000

    Step-4

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Notes and Todo App ",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Created a versatile application enabling users to Create, Read, Update, and Delete (CRUD) notes in the cloud, seamlessly linked to their email accounts.",
+    url: "https://github.com/Omkar-t06/MyNotes-project",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Random Names Generator App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+      "Designed an engaging app that randomly generates names, allowing users to like specific words and conveniently access their favorites in a dedicated tab.      ",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
-    title: "My Resume Site",
+    title: "Chat Application",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Developed a fundamental chat application in Flutter, showcasing a foundational understanding of widgets and routing across different screens.",
+    url: "https://github.com/Omkar-t06/chat_app",
   },
 ];
 
