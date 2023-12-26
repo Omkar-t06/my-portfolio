e3acb30a0dcbc7eafe3650a241c84b07d217d79d
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2023-12-26 12:58:43 +0000

    Step-3

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Engineer student studying at Vignana Bharathi Institute of Technology. I have passion for developing and learning new technologies.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Flutter development",
+  "C",
+  "Python",
+  "Java",
   "Mobile user interfaces",
-  "Graphic design",
+  "HTML-CSS",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am deeply passionate about application development and the dynamic landscape of technology. Currently, I am actively engaged in learning and mastering Flutter. This journey allows me to continuously update and expand my knowledge, ensuring that I am well-equipped to contribute to and thrive in the ever-evolving field of technology.";
 
 const About = () => {
   return (
